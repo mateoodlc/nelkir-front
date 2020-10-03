57f90111a771c964287fea084a32b03027f840a5
Mateo Orobio <mateo.orobio@idfive.com>
2020-10-02 23:09:41 -0500

    feat: project set and desktop menu built

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <div class="max-bound">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
